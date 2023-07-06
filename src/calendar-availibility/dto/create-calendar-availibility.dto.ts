dd38ad3b7adefb9382f47e7912deabc18b28a48a
Puneet Agarwal <Puneet.agarwal@dair.cloud>
2023-07-06 18:22:11 +0530

    Improved documentation

--- src/calendar-availibility/dto/create-calendar-availibility.dto.ts
+++ src/calendar-availibility/dto/create-calendar-availibility.dto.ts
@@ -1,20 +1,14 @@
-import { Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-import { Role } from '../../roles/entities/role.entity';
 import {
-    IsEmail,
-    IsNotEmpty,
-    IsOptional,
-    MinLength,
-    Validate,
+    IsNotEmpty
 } from 'class-validator';
 
 export class CreateCalendarAvailibilityDto {
-    @ApiProperty({ example: 'availibility rule' })
+    @ApiProperty({ example: 'RRULE for setting availibility  for everyday from 9:00AM IST, eg: DTSTART:20230705T165200Z\nRRULE:FREQ=DAILY;UNTIL=20231130T165200Z;INTERVAL=1;WKST=MO;BYHOUR=3;BYMINUTE=30;BYSECOND=0' })
     @IsNotEmpty()
     availableRrule: String;
 
-    @ApiProperty({ example: '30' })
+    @ApiProperty({ example: 'In minutes, eg: 30' })
     @IsNotEmpty()
     duration: Number;
 }
